360f7024e94aa923cdaddeb8cfd0f0c09e01a990
henryoung15 <ogbuhenry.o@gmail.com>
2022-09-14 17:12:06 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isalpha - function to check is your papa is alpha
- * @c: c is the int that will check your papa
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
+ *@c: is the int that will check your papa
  * Return: 0
  */
 int _isalpha(int c)
