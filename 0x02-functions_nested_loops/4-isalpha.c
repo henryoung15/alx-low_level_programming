3d5178b6ecd9d39f62b82b7515ace3e220e71b9b
henryoung15 <ogbuhenry.o@gmail.com>
2022-09-14 17:06:28 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _is_alpha - function to check is your papa is alpha
+ * _isalpha - function to check is your papa is alpha
  * @c: c is the int that will check your papa
  * Return: 0
  */
-int _isalphabet(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
